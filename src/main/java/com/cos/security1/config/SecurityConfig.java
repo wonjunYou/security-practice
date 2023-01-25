c3f2a4decf4f5407f29e65e8c37561e0748008aa
wonjunYou <youwjune@gmail.com>
2023-01-25 16:56:26 +0900

    feat : gitignore에 oauth 설정 파일 추가

--- src/main/java/com/cos/security1/config/SecurityConfig.java
+++ src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -12,7 +12,6 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     // 해당 메서드의 리턴되는 오브젝트를 IoC로 등록.
-    @Bean
     public BCryptPasswordEncoder encodePassword() {
         return new BCryptPasswordEncoder();
     }
